f84198fba57f555fac8af7fc678653a424885da3
Dmitry Nichiporenko <d.nichiporenko@zerosum.world>
2024-11-07 22:45:38 +0300

    redux config & other

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,56 @@
 import React, { useState } from "react";
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSideBarCollapsed } from "@/app/state";
+
+export const sidebarLinks = [
+  { icon: Home, label: "Home", href: "/" },
+  { icon: Briefcase, label: "Timeline", href: "/timeline" },
+  { icon: Search, label: "Search", href: "/search" },
+  { icon: Settings, label: "Settings", href: "/settings" },
+  { icon: User, label: "Users", href: "/users" },
+  { icon: Users, label: "Teams", href: "/teams" },
+];
+
+export const sidebarPriorityLinks = [
+  { icon: AlertCircle, label: "Urgent", href: "/priority/urgent" },
+  { icon: ShieldAlert, label: "High", href: "/priority/high" },
+  { icon: AlertTriangle, label: "Medium", href: "/priority/medium" },
+  { icon: AlertOctagon, label: "Low", href: "/priority/low" },
+  { icon: Layers3, label: "Backlog", href: "/priority/backlog" },
+];
 
 export const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSideBarCollapsed,
+  );
+
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white ${isSideBarCollapsed ? "w-0 hidden" : "w-64"}`;
 
   return (
     <div className={sidebarClassNames}>
@@ -14,8 +59,99 @@ export const Sidebar = () => {
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             EDLIST
           </div>
+          {isSideBarCollapsed ? null : (
+            <button
+              className="py-3"
+              onClick={() =>
+                dispatch(setIsSideBarCollapsed(!isSideBarCollapsed))
+              }
+            >
+              {
+                <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+              }
+            </button>
+          )}
         </div>
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              EDROH TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        <nav>
+          {sidebarLinks.map(({ href, icon, label }) => (
+            <SidebarLink href={href} icon={icon} label={label} />
+          ))}
+        </nav>
+        <button
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+          onClick={() => setShowProjects((prev) => !prev)}
+        >
+          <span>Projects</span>
+          {showProjects ? (
+            <ChevronUp className="h-5 w-5" />
+          ) : (
+            <ChevronDown className="h-5 w-5" />
+          )}
+        </button>
+        <button
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+          onClick={() => setShowPriority((prev) => !prev)}
+        >
+          <span>Priority</span>
+          {showPriority ? (
+            <ChevronUp className="h-5 w-5" />
+          ) : (
+            <ChevronDown className="h-5 w-5" />
+          )}
+        </button>
+        {showPriority ? (
+          <>
+            {sidebarPriorityLinks.map(({ href, icon, label }) => (
+              <SidebarLink href={href} icon={icon} label={label} />
+            ))}
+          </>
+        ) : null}
       </div>
     </div>
+  );
+};
+
+interface ISidebarlinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+const SidebarLink = ({ href, icon: Icon, label }: ISidebarlinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  const screenWidth = window.innerWidth;
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        } justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
---
+++ src/app/state/api.ts
@@ -0,0 +1,10 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_UR }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
